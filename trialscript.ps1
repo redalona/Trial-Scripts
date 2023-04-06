cb64955f07fbb92ec4a8b71a986cf8d71ef917c6
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-06 13:12:15 -0700

    Update trialscript.ps1

--- trialscript.ps1
+++ trialscript.ps1
@@ -1,4 +1,4 @@
 $users = Import-Csv -Path "C:\path\to\file.csv"
 foreach ($user in $users) {
-    New-ADUser -Name $user.Name -GivenName $user.GivenName -Surname $user.Surname -Title $user.Title -Department $user.Department -SamAccountName $user.samAccountName -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force) -Enabled $true
+    New-ADUser -Name $user.Name -GivenName $user.GivenName -Surname $user.Surname -Title $user.Title -Department $user.Department -SamAccountName $user.samAccountName -Path "OU=$user.Department DC=corp,DC=greendata,DC=com"
 }
